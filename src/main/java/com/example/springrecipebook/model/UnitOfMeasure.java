ce9423f6a83556c9abab45546e6795c85aa5982a
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2020-11-12 20:17:42 +0000

    Add UnitOfMeasure and define one-to-one relationship to Ingredient

---
+++ src/main/java/com/example/springrecipebook/model/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package com.example.springrecipebook.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String unitName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+
+}
