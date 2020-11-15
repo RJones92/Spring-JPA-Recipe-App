cdd2bb23f3f3f32f0f40764495376ba0b2f6ea40
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2020-11-15 14:19:25 +0000

    Database initialisation for Category and UnitOfMeasure

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (unit_name) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (unit_name) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (unit_name) VALUES ('Litre');
+INSERT INTO unit_of_measure (unit_name) VALUES ('Pinch');
+INSERT INTO unit_of_measure (unit_name) VALUES ('Ounce');
